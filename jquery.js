8a1a0e376ec5d916dc6aedfa777798a34c15efe2
kou665 <hjk89998@yahoo.co.jp>
2015-06-16 22:25:47 +0900

    commit

--- jquery.js
+++ jquery.js
@@ -7,34 +7,42 @@ $(function () {
                 return;
             }
             var category_name = ids[index];
-            for(var i = 0;i<5;i++){
+            for(var i = 0;i<article_numbers;i++){
                 var link = $("#"+category_name+String(i)+" a").attr("href");
                 relatedPage(link,index,i);
             }
             check[index]=true;
         }
     });
-})
-var check =[true,false,false,false,false,false,false,false,false,false]
-var ids = ["top", "dom", "int", "eco", "ent", "spo", "52", "gourmet", "love", "trend"]
-var tab_categories = ["topics/rss/top", "topics/rss/dom", "topics/rss/int", "topics/rss/eco", "topics/rss/ent", "topics/rss/spo", "rss/summary/52", "topics/rss/gourmet", "topics/rss/love", "topics/rss/trend"]
+});
+var relatedNews_numbers=3;
+var article_numbers=5;
+var check =[];
+var ids = ["top", "dom", "int", "eco", "ent", "spo", "52", "gourmet", "love", "trend"];
+var tab_categories =ids.map(function(x){
+    if(x==="52"){
+        return "rss/summary/"+x;
+    }else{
+        return "topics/rss/"+x;
+    }
+});
 google.load("feeds", "1");
 function initialize(){
     //フィードの取得
     for (var index = 0; index < tab_categories.length; index++) {
         var feed = new google.feeds.Feed("http://news.livedoor.com/" + tab_categories[index] + ".xml");
         //エントリの表示数の設定
-        feed.setNumEntries(5);
+        feed.setNumEntries(article_numbers);
         feed.includeHistoricalEntries();
         //関数の定義
         (function (index) {
-            feed.load(function (result, i) {
+            feed.load(function (result) {
                 if (!result.error) {
                     //読み込みが成功したときの処理
                     var container = document.getElementById(ids[index]);
                     var htmlstr = "";
-                    for (var i = 0,result_feed_entries_length = result.feed.entries.length; i < result_feed_entries_length; i++) {
-                        $("."+ids[index]).append("<div id="+ids[index]+i+"></div>")
+                    for (var i = 0; i < result.feed.entries.length; i++) {
+                        $("."+ids[index]).append("<div id="+ids[index]+i+"></div>");
                         var entry = result.feed.entries[i];
                         //日付の取得
                         var pdate = new Date(entry.publishedDate); //Dateクラス
@@ -66,10 +74,11 @@ function initialize(){
                             htmlstr += '<div class="thum"><img ' + "src='http://news.livedoor.com/img/fb/news.png?v=20131122'" + ' width="155" height="125"></div>';
                         }
                         htmlstr += '<p class="info">' + date + '</p>';
-                        htmlstr += '<p class="tit">' + entry.title + '</p></a></div>';
+                        htmlstr += '<p class="tit">' + entry.title.trim() + '</p></a></div>';
                         //主要のみ関連を表示する。
-                        if(index==0){
-                        relatedPage(entry.link,index,i);
+                        if(index===0){
+                            check[index]=true;
+                            relatedPage(entry.link,index,i);
                         }
                         
                     }
@@ -90,9 +99,9 @@ google.setOnLoadCallback(initialize);
 function relatedPage(link,category,article_num) {
     $.get(link,function (data) {
         //タグを一つずつ見る
-        var tags = $(data.responseText).find(".articleHeadKeyword  a").map(function () { return $(this).attr("href") } );
+        var tags = $(data.responseText).find(".articleHeadKeyword  a").map(function () { return $(this).attr("href"); } );
         //タグから一つずつ関連記事を載せる
-        for (var i = 0; i<3 ; i++) {
+        for (var i = 0; i<relatedNews_numbers ; i++) {
             relatedNews(tags[i % tags.length],category,article_num);
         }
     });
@@ -104,24 +113,24 @@ function relatedPage(link,category,article_num) {
 function relatedNews(link,category,article_num) {
     $.get(link,function (data) {
         var articles = $(data.responseText).find(".articleList");
-        var links = articles.find("a").map(function () {
-            return $(this).attr("href")
-        })
+        var links = articles.find("a:even").map(function () {
+            return $(this).attr("href");
+        });
         var titles = articles.find("h3").map(function () {
-            return $(this).text()
-        })
+            return $(this).text();
+        });
         
         var related_articles = $("#"+ids[category]+article_num);
         
-        var displayed = related_articles.find("p").map(function(){return $(this).text().replace(/関連記事:/g,"")});
-        loop:for (var i = 0,links_length=links.length; i < links_length; i++) {
-            var link = links[i];
-            var title = titles[i];
-            //すでに載っていれば飛ばす
-            for(var j=0,displayed_length=displayed.length; j<displayed_length ; j++){
-                if(title==displayed[j]) continue loop;
-            }
-            related_articles.append('<a href="'+link+'" target="_blank"><p><font size="2" color="Blue">'+"関連記事:"+title)
+        var displayed = related_articles.find("p").map(function(){return $(this).text().replace(/関連記事:/g,"");}).get();
+        for (var i = 0; i < links.length; i++) {
+            var newlink = links[i];
+            
+            var title = titles[i].trim();
+            
+            if(displayed.indexOf(title)>=0) continue ;
+            
+            related_articles.append('<a href="'+newlink+'" target="_blank"><p><font size="2" color="Blue">'+"関連記事:"+title);
             return;
         }
     });
